efb531b2d9377056845d05268c11d079e051766d
larav1 <l.vitorinha09@gmail.com>
2025-01-13 17:13:28 -0300

    Ajuste no css

--- scripts/fontes.js
+++ scripts/fontes.js
@@ -1,3 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.querySelector(".menu-icon");
+  const menuContent = document.querySelector(".menu-content");
+
+  menuIcon.addEventListener("click", () => {
+    menuContent.classList.toggle("menu-active");
+  });
+
+  // Fecha o menu se o usuário clicar fora do menu em telas pequenas.
+  document.addEventListener("click", function (event) {
+    if (
+      !event.target.closest(".navbar") &&
+      !event.target.closest(".menu-content") &&
+      window.innerWidth < 769
+    ) {
+      menuContent.classList.remove("menu-active");
+    }
+  });
+});
+
 function mostrarInformacoes(tipo) {
   // Esconde todas as seções
   const mitologias = document.querySelectorAll(
@@ -5,6 +25,7 @@ function mostrarInformacoes(tipo) {
   );
   mitologias.forEach((el) => (el.style.display = "none"));
 
+  // Mostra a mitologia selecionada
   if (tipo === "grega") {
     document.querySelector(".mitologia-grega").style.display = "block";
   } else if (tipo === "romana") {
