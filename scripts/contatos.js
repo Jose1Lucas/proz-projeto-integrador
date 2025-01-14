3cbc542ba880af06ee51d8e158b4449b254ff406
larav1 <l.vitorinha09@gmail.com>
2025-01-14 13:43:01 -0300

    JS de contatos

---
+++ scripts/contatos.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.querySelector(".menu-icon");
+  const menuContent = document.querySelector(".menu-content");
+
+  menuIcon.addEventListener("click", () => {
+    menuContent.classList.toggle("menu-active");
+  });
+
+  // Fecha o menu se o usuário clicar fora do menu em telas pequenas.
+  document.addEventListener("click", function (event) {
+    if (
+      !event.target.closest(".navbar") &&
+      !event.target.closest(".menu-content") &&
+      window.innerWidth < 769
+    ) {
+      menuContent.classList.remove("menu-active");
+    }
+  });
+});
